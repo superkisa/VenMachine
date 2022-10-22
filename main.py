e937a5c2a1934d450e4aa77263d3ea71ea775efe
Александра Кулешова <81680387+superkisa@users.noreply.github.com>
2022-10-22 20:46:24 +0000

    finish

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Classing with homie 💞💖
+# Classing with 💞💖
 from vending.vending import Item, VendingMachine
 
 apple = Item("🍎", 10)
@@ -11,22 +11,9 @@ ven1.fill_cell(apple, 0, 1)
 print(ven1.dict_items)
 print("\n`````````````````\n")
 
-# test = {'apple': [{"x": 0, "y": 0, "depth": 8}, {"x": 0, "y": 1, "depth": 8}]}
-
-# test["apple"].append({"x": 0, "y": 2, "depth": 8})
-
-# print(test["apple"])
-
 ven1.present_cells()
-# for i in range(17):
-#     ven1.buying_item(apple)
+
 ven1.buying_item(apple)
 
-# ven1.empty_all()
-# ven1.present_cells()
-# ven1.buying_item(apple)
-# dick = {"big": [25, 30, 40], "small": "🍆"}
-# dick["black"] = 12
-# dick.update({"big": 50})
-# print(dick)
-# print("big" in dick.keys())
+print(VendingMachine.show_methods())
+
--- vending/vending.py
+++ vending/vending.py
@@ -1,4 +1,15 @@
 class Item:
+    """This class is used for determining the goods that
+    we can put in our vending machine.
+
+    Attributes:
+        name(str): name of the article
+        price(float): price of the article
+
+    Methods:
+        get_price(): Price getter
+    """
+
     def __init__(self, name: str, price: float):
         self.name = name
         self.price = price
@@ -7,11 +18,49 @@ class Item:
         return f"Item(name = '{self.name}', price = {self.price})"
 
     def get_price(self) -> float:
+        """Price getter."""
         return self.price
 
 
-class VendingMachine:
+class VendingMachine:  # And this is our vending machine.
+    """The class implementing a vending machine
+
+    Attributes:
+    width
+    length
+    depth
+    currency
+    wallet
+    dict_items
+
+    
+    Methods:
+        deposit_credits(amount)
+            This method is used deposit credits to vending machine's internal wallet.
+        withdraw_credits(amount)
+            This method is used to safely withdraw credits from
+            vending machine's internal wallet.
+        fill_cell(item, i, j)
+            This method is used to place items in cell_ij of the vending machine.
+        present_cells()
+            Show contents of non-empty cells and.
+        empty_all()
+            Empty all cells.
+        buying_item(item)
+            The method used for buying items from vending machine and money exchange.
+        show_methods()
+            (classmethod) Print all availible methods of this class.
+    """
+
     def __init__(self, width: int, length: int, depth: int, currency: str = "\u20BD"):
+        """Initializes vending machine with specified characteristics.
+
+        Args:
+            width
+            length
+            depth
+            currency
+        """
         self.width = width
         self.length = length
         self.depth = length
@@ -36,6 +85,11 @@ class VendingMachine:
         )
 
     def deposit_credits(self, amount: float) -> None:
+        """This method is used deposit credits to vending machine's internal wallet.
+
+        Args:
+            amount(float) - amount to deposit
+        """
         if amount >= 0:
             self.wallet += amount
             return None
@@ -43,6 +97,12 @@ class VendingMachine:
             raise ValueError("`amount` must be a positive number.")
 
     def withdraw_credits(self, amount: float) -> None:
+        """This method is used to safely withdraw credits from
+        vending machine's internal wallet.
+
+        Args:
+            amount(float) - amount to withdraw
+        """
         if amount >= 0:
             if self.wallet >= amount:
                 self.wallet -= amount
@@ -58,6 +118,15 @@ class VendingMachine:
         pass
 
     def fill_cell(self, item: Item, i: int, j: int) -> dict:
+        """This method is used to place items in cell_ij of the vending machine
+        
+        Args:
+            item(Item): the item to be placed
+            i(int)
+            j(int)
+        Returns:
+            self.dict_item(dict)
+        """
         if i > self.length - 1 or j > self.width - 1 or i < 0 or j < 0:
             print("Error: not currect i or j")
         else:
@@ -69,20 +138,27 @@ class VendingMachine:
             return self.dict_items
 
     def present_cells(self):
+        """Show contents of non-empty cells and current balance."""
         if self.dict_items == {}:
             print("Vending machine is empty!")
         else:
             for i in self.dict_items.keys():
-                print(i)
+                print(f"{i} =>")
                 for j in self.dict_items[i]:
                     print("    ", j)
+        print(f'\n current balance: {self.wallet} {self.currency}.\n')
 
     def empty_all(self):
+        """Empty all cells."""
         self.dict_items = {}
 
     def buying_item(self, item: Item):
+        """The method used for buying items from vending machine and money exchange.
+
+        Args:
+            item(Item): Item object for purchase
+        """
         a = 0
-        # a += i['fillness of the sell'] for i in self.dict_items[item.name]
         if self.dict_items == {}:
             print("You can't buy anything now. Vending machine is empty.")
         else:
@@ -107,3 +183,7 @@ class VendingMachine:
                         i["repletion of the cell"] -= 1
                         break
                 print(f"All ok! Don't forget your {item.name}!")
+
+    @classmethod
+    def show_methods(cls):
+        print(VendingMachine.__doc__)
